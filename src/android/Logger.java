536970d05f704b07a0a91024bf3c088f4311412c
Raitha Karunarathne <rajitha.k@synergentl.com>
2015-06-18 14:24:08 +0530

    Added ZIP compression

--- src/android/Logger.java
+++ src/android/Logger.java
@@ -9,13 +9,18 @@ import org.json.JSONObject;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.*;
+import java.util.zip.*;
 import java.io.IOException;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Logger extends CordovaPlugin {
-    private static BufferedWriter bufferedWriter;
+    // private static BufferedWriter bufferedWriter;
+    private static FileOutputStream fos;
+    private BufferedOutputStream bos;
+    private ZipOutputStream zos;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -26,25 +31,28 @@ public class Logger extends CordovaPlugin {
         else if(action.equals("write")){
             // this.write(args.getString(0),callbackContext);
             class threaded implements Runnable{
-                BufferedWriter budWriter;
+                // BufferedWriter budWriter;
+                ZipOutputStream zipWriter;
                 CallbackContext cbx;
                 String data;
 
-                threaded(String d,BufferedWriter bw, CallbackContext c){
-                    this.budWriter = bw;
+                // threaded(String d,BufferedWriter bw, CallbackContext c){
+                threaded(String d,ZipOutputStream zw, CallbackContext c){
+                    // this.budWriter = bw;
+                    this.zipWriter = zw;
                     this.cbx = c;
                     this.data = d;
                 }
                 public void run(){
                     try{
-                        this.budWriter.write(data);
+                        this.zipWriter.write(data.getBytes());
                         cbx.success("Written successfully");
                     } catch(Exception e){
                         cbx.error(e.toString());
                     }
                 }
             }
-            cordova.getThreadPool().execute(new threaded(args.getString(0),this.bufferedWriter,callbackContext));
+            cordova.getThreadPool().execute(new threaded(args.getString(0),this.zos,callbackContext));
             return true;
         }
         else if(action.equals("close")){
@@ -54,12 +62,17 @@ public class Logger extends CordovaPlugin {
         return false;
     }
 
-    public void initialize(String filename, CallbackContext callbackContext){
+    public void initialize(String fileName, CallbackContext callbackContext){
         try {
-            this.bufferedWriter = new BufferedWriter(new FileWriter("/sdcard/"+filename,false));
+            // this.bufferedWriter = new BufferedWriter(new FileWriter("/sdcard/"+filename,false));
             // this.bufferedWriter.write("x,y");
             // this.bufferedWriter.write(content);
             // this.bufferedWriter.close();
+            this.fos = new FileOutputStream("/sdcard/" + fileName + ".zip");
+            this.bos = new BufferedOutputStream(fos);
+            this.zos = new ZipOutputStream(bos);
+            zos.putNextEntry( new ZipEntry(fileName + ".txt"));
+
             callbackContext.success("File Opened");
 
         } catch (IOException e) {
@@ -78,7 +91,9 @@ public class Logger extends CordovaPlugin {
 
     private void close(CallbackContext callbackContext) {
         try{
-            this.bufferedWriter.close();
+            // this.bufferedWriter.close();
+            zos.closeEntry();
+            zos.close();
             callbackContext.success("Closed");
         } catch(IOException e){
             callbackContext.error(e.getMessage());
